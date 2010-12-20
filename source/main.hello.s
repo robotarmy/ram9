23a7c5466d6cabd8b1eb9362233d39177d2ad40a
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-19 17:10:09 -0800

    still trying to answer the riddle

--- source/main.hello.s
+++ source/main.hello.s
@@ -1,25 +1,36 @@
 	.cpu arm7tdmi
   .file "main.hello.s"
-  .align 2
-  .data
-.LhelloStr:
-  .align 2
-  .asciz "Hello World!"
 	.text
-	.align	2
+	.align	
 	.global	main
 	.type	main, %function
+  .func main
 main:
 	stmfd	sp!, {fp, lr}
 	add	fp, sp, #4
 	bl	consoleDemoInit
+  .print "somthing broken"
 	ldr	r0, .Lhello
-	bl	iprintf
+	bl	printf
 .L2:
 	bl	swiWaitForVBlank
 	b	.L2
+  .endfunc
+.LhelloStr:
+  .if 1
+    .align
+    .string "Hello World!"
+  .elseif 0
+    .align
+    .asciz "123456781AB"  @11BYTES will print 12 will not
+    .space 1 @ simulate 12 bytes - will not print
+  .elseif 0
+    .space 5  @ simulate 16 bytes
+  .elseif 0
+    .space 21 @ simulate 32  bytes
+  .endif
 .Lhello:
-  .align 2
+  .align
   .word .LhelloStr
   .ident "RAM: 333 ARM"
 
