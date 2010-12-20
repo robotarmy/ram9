79b13bb9cfbfdb600a3798a4680281bc0a4aded5
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-19 16:53:31 -0800

    why is this  hello world broken?

--- source/main.hello.s
+++ source/main.hello.s
@@ -1,11 +1,6 @@
 	.cpu arm7tdmi
   .file "main.hello.s"
-	.section .rodata
   .align 2
-.SPC:
-  .string " "
-.WORLD:
-	.ascii	"World!\000"
 	.text
 	.align	2
 	.global	main
@@ -16,25 +11,14 @@ main:
 	bl	consoleDemoInit
 	ldr	r0, .Lhello
 	bl	iprintf
-	ldr	r0, .LSP
-	bl	iprintf
-	ldr	r0, .Lworld
-	bl	iprintf
 .L2:
 	bl	swiWaitForVBlank
 	b	.L2
 .LhelloStr:
-  .align 2
-  .string "hello"
-.LSP:
-  .align 2
-  .word .SPC @memory address
-  .align 2
-.Lworld:
-  .word .WORLD
-	.align	2
+  .ascii "Hello World!\000"
 .Lhello:
-	.word	.LhelloStr
+  .align 2
+  .word .LhelloStr
 	.size	main, .-main
   .ident "RAM: 333 ARM"
 
