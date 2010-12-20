e17b6b178540b7b1cf016102294893750fe021bc
Curtis Schofield <github@robotarmyma.de>
2010-12-19 20:11:21 -0800

    ldr version

--- source/main.hello.s
+++ source/main.hello.s
@@ -1,27 +1,17 @@
   .cpu arm7tdmi
-  .file "main.hello.s"
-  .data
   .text
-  .align	
   .global	main
   .type	main, %function
-.func main
-main:
-  stmfd	sp!, {fp, lr}
-  add	fp, sp, #4
+  main:
   bl	consoleDemoInit
   ldr r0, .Lhello
-  bl	printf
-  adr r0, .LhelloStr
   bl	printf
   .L2:
   bl	swiWaitForVBlank
   b	.L2
-.endfunc
 .LhelloStr:
-  .asciz "Hello World!"
+  .string "Hello World!"
   .align 2
 .Lhello:
   .word .LhelloStr
-  .ident "RAM 333 ARM"
-  .end
+
