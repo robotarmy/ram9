8b4437eae76bbf86e86c00816a8408e9c5bc3685
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-19 17:22:47 -0800

    Hello World in the text section vi use of adr

--- source/main.hello.s
+++ source/main.hello.s
@@ -1,5 +1,6 @@
 	.cpu arm7tdmi
   .file "main.hello.s"
+  .data
 	.text
 	.align	
 	.global	main
@@ -9,28 +10,18 @@ main:
 	stmfd	sp!, {fp, lr}
 	add	fp, sp, #4
 	bl	consoleDemoInit
-  .print "somthing broken"
-	ldr	r0, .Lhello
+  .print "somthing broken" @desmune may need to be recompiled with -DDEVELOPER
+  adr r0, .LhelloStr
 	bl	printf
 .L2:
 	bl	swiWaitForVBlank
 	b	.L2
   .endfunc
 .LhelloStr:
-  .if 1
-    .align
-    .string "Hello World!"
-  .elseif 0
-    .align
-    .asciz "123456781AB"  @11BYTES will print 12 will not
-    .space 1 @ simulate 12 bytes - will not print
-  .elseif 0
-    .space 5  @ simulate 16 bytes
-  .elseif 0
-    .space 21 @ simulate 32  bytes
-  .endif
+  .align
+  .asciz "Hello World!"
 .Lhello:
   .align
   .word .LhelloStr
-  .ident "RAM: 333 ARM"
-
+  .ident "RAM 333 ARM"
+  .end
